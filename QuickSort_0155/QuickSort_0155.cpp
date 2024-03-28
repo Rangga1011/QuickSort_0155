2532c427f6939a72aff4e313fb1e21a03672ea36
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-03-28 09:35:52 +0700

    Menambahkan void q_short step 6

--- QuickSort_0155/QuickSort_0155.cpp
+++ QuickSort_0155/QuickSort_0155.cpp
@@ -53,7 +53,8 @@ void q_short(int low, int high)
       // search for an element greather than pivot
         while ((arr[i] <= pivot) && (i <= high)) // step 5
         {
-
+            i++; // step 6
+            cmp_count++;
         }
       
     }
